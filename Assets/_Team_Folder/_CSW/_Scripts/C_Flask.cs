0a076cc3593ba8b1c3ec3762284e98599ad37dd0
Tiha123 <waits6262@gmail.com>
2025-08-13 15:32:31 +0900

    v0.0.3]feat:TutorialScene

--- Assets/_Team_Folder/_CSW/_Scripts/C_Flask.cs
+++ Assets/_Team_Folder/_CSW/_Scripts/C_Flask.cs
@@ -6,45 +6,7 @@ using UnityEngine;
 public class C_Flask : MonoBehaviour
 {
     [SerializeField] private List<LiquidData_L> liquidData=new List<LiquidData_L>();
-    [SerializeField] private Material liquidMaterial;
     
     private Ch_VelocityInteractable velocityInteractable;
-    private ParticleSystem particleSystem;
     private bool combineFailed = false;
-
-    private void Awake()
-    {
-        particleSystem=GetComponentInChildren<ParticleSystem>();
-    }
-
-    private void Update()
-    {
-        if (velocityInteractable.velocity.magnitude > 0.06f)
-        {
-            
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Pippet"))
-        {
-            
-        }
-    }
-
-    void OnParticleCollision(GameObject other)
-    {
-        if (other.CompareTag("Pippet"))
-        {
-            if (liquidData.Contains(GetComponent<C_Pipette>().LiquidData))
-            {
-                
-            }
-            else
-            {
-                combineFailed = true;
-            }
-        }
-    }
 }
