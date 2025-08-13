214a466667b70cc999c4452853dff06b8badbf9b
HyejeongOk <oghyejeong2@gmail.com>
2025-08-13 15:31:26 +0900

    v0.0.5 ] fix : Tube - 액체 붓기 최적화

--- Assets/_Team_Folder/_OHJ/3.Script/Behaviour/Fill.cs
+++ Assets/_Team_Folder/_OHJ/3.Script/Behaviour/Fill.cs
@@ -6,25 +6,61 @@ public class Fill : MonoBehaviour
 {
     public float filling = 0.1f;
     public float stopPos = 0.5f;
-    public Vector3 pos = Vector3.zero;
+    public Vector3 pos;
+    public Vector3 Startpos;
     public bool isfilling = false;
+
+    public GameObject gelObj;
+
+    private void Awake()
+    {
+        Startpos = transform.position;
+    }
 
     private void Start()
     {
         pos = transform.position;
+        StartCoroutine(Fill_co());
     }
 
-    private void Update()
+    private IEnumerator Fill_co()
     {
-        if(isfilling)
+        while (true)
         {
-            pos.y += filling * Time.deltaTime;
-
-            if(pos.y > stopPos)
+            if (isfilling)
             {
-                pos.y = stopPos;
+                pos.y += filling * Time.deltaTime;
+
+                if (pos.y > stopPos)
+                {
+                    pos.y = stopPos;
+                    yield break;
+                }
+                transform.position = pos;
             }
-            transform.position = pos;
+
+            yield return null;
         }
+    }
+
+    //private void Update()
+    //{
+    //    if(isfilling)
+    //    {
+    //        pos.y += filling * Time.deltaTime;
+
+    //        if(pos.y > stopPos)
+    //        {
+    //            pos.y = stopPos;
+    //        }
+    //        transform.position = pos;
+    //    }
+    //}
+
+    //private IEnumerator
+
+    public void OriginPos()
+    {
+        transform.position = Startpos;
     }
 }
--- Assets/_Team_Folder/_OHJ/3.Script/Object/Tube.cs
+++ Assets/_Team_Folder/_OHJ/3.Script/Object/Tube.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Tube : MonoBehaviour
 {
     public Fill fill;
-    public Material mat;
+    private Material mat;
 
     private GameObject ParticleObj;
     public ParticleSystem particle;
@@ -33,86 +33,160 @@ public class Tube : MonoBehaviour
         {
             Debug.Log("fill이 연결되지 않았습니다");
         }
-        //Transform leak = transform.Find("WaterPoint");
-        //if (leak == null)
-        //{
-        //    Debug.Log("Cylinder 오브젝트를 찾을 수 없습니다.");
-        //    return;
-        //}
+
         ParticleObj = transform.Find("WaterPoint").GetChild(0).gameObject;
         ParticleObj.TryGetComponent(out particle);
         childParticle = ParticleObj.transform.GetChild(0);
     }
 
-    private void Update()
+    private void Start()
     {
-        float angle = transform.rotation.eulerAngles.z;
+        StartCoroutine(Pour_co());
+    }
 
-        // 각도가 기준을 넘으면 파티클 생성
-        if ((angle > MinThreshold && angle < MaxThreshold) || (angle < -MinThreshold && angle > -MaxThreshold))
+    private IEnumerator Pour_co()
+    {
+        while(true)
         {
-            isPour = true;
+            float angle = transform.rotation.eulerAngles.z;
 
-            fillValue = mat.GetFloat("_Fill");
-            Debug.Log($"현재 양 : {fillValue}");
-            fillValue  -= PourValue;
-            mat.SetFloat("_Fill", fillValue);
-            Debug.Log($"fillValue  감소 중 : {fillValue}");
-            //Debug.Log($"액체 흘려나옵니다 angle = {angle}");
+            // 각도가 기준을 넘으면 파티클 생성
+            if ((angle > MinThreshold && angle < MaxThreshold) || (angle < -MinThreshold && angle > -MaxThreshold))
+            {
+                isPour = true;
+
+                fillValue = mat.GetFloat("_Fill");
+                fillValue -= PourValue;
+                mat.SetFloat("_Fill", fillValue);
+
+                Debug.Log($"현재 양 : {fillValue}");
+                Debug.Log($"액체 흘려나옵니다 angle = {angle}");
+
+
+                if (!ParticleObj.activeInHierarchy)
+                {
+                    ParticleObj.SetActive(true);
+                }
+
+                if (!particle.isPlaying)
+                {
+                    particle.Play();
+                }
+
+                if (fillValue < -1f)
+                {
+                    fillValue = -1f;
+                    if (ParticleObj.activeInHierarchy)
+                    {
+                        ParticleObj.SetActive(false);
+                    }
+
+                    fill.isfilling = false;
+                    isPour = false;
+
+                    particle.Stop();
+                    yield break;
+                }
+
+                // Raycast 
+                //Physics.Raycast(Ray ray, out RaycastHit hitInfo)
+                //충돌이 발생 시 hitinfo에 저장 => hitinfo가 gelTray라면 fill 되도록
+                Ray ray = new Ray();
+                ray.origin = ParticleObj.transform.position;
+                RaycastHit hit;
+                ray.direction = Vector3.down;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == gelTray)
+                    {
+                        fill.isfilling = true;
+                    }
+
+                    else
+                    {
+                        fill.isfilling = false;
+                    }
+                }
+            }// end  : 각도 충족할 때
+
+            else
+            {
+                StopPouring();
+            }
+            yield return null;
+        }
+    }
+
+    //private void Update()
+    //{
+    //    float angle = transform.rotation.eulerAngles.z;
+
+    //    // 각도가 기준을 넘으면 파티클 생성
+    //    if ((angle > MinThreshold && angle < MaxThreshold) || (angle < -MinThreshold && angle > -MaxThreshold))
+    //    {
+    //        isPour = true;
+
+    //        fillValue = mat.GetFloat("_Fill");
+    //        Debug.Log($"현재 양 : {fillValue}");
+    //        fillValue  -= PourValue;
+    //        mat.SetFloat("_Fill", fillValue);
+    //        Debug.Log($"fillValue  감소 중 : {fillValue}");
+    //        //Debug.Log($"액체 흘려나옵니다 angle = {angle}");
 
             
-            if(!ParticleObj.activeInHierarchy)
-            {
-                ParticleObj.SetActive(true);
-            }
+    //        if(!ParticleObj.activeInHierarchy)
+    //        {
+    //            ParticleObj.SetActive(true);
+    //        }
 
-            if(!particle.isPlaying)
-            {
-                particle.Play();
-            }
+    //        if(!particle.isPlaying)
+    //        {
+    //            particle.Play();
+    //        }
 
-            if(fillValue < -1f)
-            {
-                fillValue = -1f;
-                if (ParticleObj.activeInHierarchy)
-                {
-                    ParticleObj.SetActive(false);
-                }
+    //        if(fillValue < -1f)
+    //        {
+    //            fillValue = -1f;
+    //            if (ParticleObj.activeInHierarchy)
+    //            {
+    //                ParticleObj.SetActive(false);
+    //            }
 
-                fill.isfilling = false;
-                isPour = false;
+    //            fill.isfilling = false;
+    //            isPour = false;
 
-                particle.Stop();
-            }
+    //            particle.Stop();
+    //        }
 
-            // Raycast 
-            //Physics.Raycast(Ray ray, out RaycastHit hitInfo)
-            //충돌이 발생 시 hitinfo에 저장 => hitinfo가 gelTray라면 fill 되도록
-            Ray ray = new Ray();
-            ray.origin = ParticleObj.transform.position;
-            RaycastHit hit;
-            ray.direction = Vector3.down;
+    //        // Raycast 
+    //        //Physics.Raycast(Ray ray, out RaycastHit hitInfo)
+    //        //충돌이 발생 시 hitinfo에 저장 => hitinfo가 gelTray라면 fill 되도록
+    //        Ray ray = new Ray();
+    //        ray.origin = ParticleObj.transform.position;
+    //        RaycastHit hit;
+    //        ray.direction = Vector3.down;
 
-            if(Physics.Raycast(ray, out hit))
-            {
-                if(hit.transform == gelTray)
-                {
-                    fill.isfilling = true;
-                }
+    //        if(Physics.Raycast(ray, out hit))
+    //        {
+    //            if(hit.transform == gelTray)
+    //            {
+    //                fill.isfilling = true;
+    //            }
 
-                else
-                {
-                    fill.isfilling = false;
-                }
-            }
-        }// end  : 각도 충족할 때
+    //            else
+    //            {
+    //                fill.isfilling = false;
+    //            }
+    //        }
+    //    }// end  : 각도 충족할 때
 
-        else
-        {
-            StopPouring();
-        }
+    //    else
+    //    {
+    //        StopPouring();
+    //    }
 
-    }
+    //}
 
     // 붓기 멈추기
     private void StopPouring()
