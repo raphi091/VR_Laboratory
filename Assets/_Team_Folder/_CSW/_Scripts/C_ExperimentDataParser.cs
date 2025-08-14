4a7c3ce93e831c5f41413ce21a6c7779007c66df
Tiha123 <waits6262@gmail.com>
2025-08-14 13:11:53 +0900

    v0.0.5]feat:DataParse

--- Assets/_Team_Folder/_CSW/_Scripts/C_ExperimentDataParser.cs
+++ Assets/_Team_Folder/_CSW/_Scripts/C_ExperimentDataParser.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public enum propType
+public enum ToolType
 {
     None=0,
     Flask=1,
@@ -16,14 +18,23 @@ public class C_ExperimentDataParser : Ch_BehaviourSingleton<C_ExperimentDataPars
     {
         return true;
     }
+    
+    public ParseEventArgs ParseEventArgs;
+    
+    public UnityEvent<ParseEventArgs> DataParsed=new UnityEvent<ParseEventArgs>();
 }
 
-public interface C_ExperimentProp
+public interface C_ExperimentTool
 {
-    public void ImportLiquidData(LiquidData_L liquidData);
-    
-    public LiquidData_L ExportLiquidData();
-    public LiquidData_L ExportLiquidData(LiquidData_L liquidData);
-    
-    public List<LiquidData_L> ExportLiquidDataList();
+    public bool IsWritable { get; set; }
+    public ToolType ToolType { get; set; }
+    public void ImportLiquidData(List<LiquidData_L> liquidDatas);
+    public List<LiquidData_L> ExportLiquidDatas();
+    public void ClearData();
+}
+
+public class ParseEventArgs : EventArgs
+{
+    public C_ExperimentTool fromTool;
+    public C_ExperimentTool toTool;
 }
