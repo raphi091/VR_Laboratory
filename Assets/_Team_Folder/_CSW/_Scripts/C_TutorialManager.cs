a72dc1b729e37b4ae72cee325b0fcb34008bc433
Tiha123 <waits6262@gmail.com>
2025-08-19 17:54:46 +0900

    v0.0.8]feat:TutorialFlowOnprocess

--- Assets/_Team_Folder/_CSW/_Scripts/C_TutorialManager.cs
+++ Assets/_Team_Folder/_CSW/_Scripts/C_TutorialManager.cs
@@ -3,20 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class C_TutorialManager : Ch_BehaviourSingleton<C_TutorialManager>
+public class C_TutorialManager : MonoBehaviour
 {
-    protected override bool IsDontdestroy()
-    {
-        return true;
-    }
+    private bool isCompleted = false;
+    private NpcController_G npc;
 
-    bool isCompleted = false;
-
-    void Start()
+    private void Awake()
     {
-        ParseEventArgs parseEventArgs=new ParseEventArgs();
-        parseEventArgs.fromTool = null;
-        parseEventArgs.toTool = null;
-        C_ExperimentDataParser.I.DataParsed.Invoke(parseEventArgs);
+        npc=FindObjectOfType<NpcController_G>();
     }
 }
