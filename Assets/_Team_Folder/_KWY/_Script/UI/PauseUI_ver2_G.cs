89c65acfeedee91949955378922fea70fff57178
raphi091 <kwy8024@gmail.com>
2025-09-01 17:21:18 +0900

    v0.0.66 ] feat: new UI

---
+++ Assets/_Team_Folder/_KWY/_Script/UI/PauseUI_ver2_G.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+
+public class PauseUI_ver2_G : MonoBehaviour
+{
+    [Header("UI Panels")]
+    public GameObject basePanel;
+    public GameObject settingPanel;
+    public GameObject baseBtn;
+    public GameObject settingSilder;
+
+    [Header("Input Settings")]
+    public InputActionReference menuAction;
+    public InputActionAsset actions;
+
+    [Header("VR Settings")]
+    public Transform mainCameraTransform;
+    public float spawnDistance = 1.5f;
+
+    [Header("Sound")]
+    public AudioClip clickbtn;
+
+    private XRInput input;
+    private bool isPasued = false;
+
+
+    private void Awake()
+    {
+        input = new XRInput();
+    }
+
+    private void OnEnable()
+    {
+        if (menuAction != null)
+        {
+            menuAction.action.performed += OnMenuButtonPressed;
+            menuAction.action.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (menuAction != null)
+        {
+            menuAction.action.performed -= OnMenuButtonPressed;
+            menuAction.action.Disable();
+        }
+    }
+
+    private void OnMenuButtonPressed(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase != InputActionPhase.Performed) return;
+
+        if (isPasued)
+            CloseAllMenus();
+        else
+            OpenMenu();
+    }
+
+    private void OpenMenu()
+    {
+        if (mainCameraTransform != null)
+        {
+            Vector3 forward = mainCameraTransform.forward;
+            forward.y = 0;
+            transform.position = mainCameraTransform.position + forward.normalized * spawnDistance;
+            float cameraYRotation = mainCameraTransform.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, cameraYRotation, 0f);
+        }
+
+        Time.timeScale = 0f;
+        basePanel.SetActive(true);
+        settingPanel.SetActive(false);
+        input.XRIUI.Enable();
+
+        SetSelectedUIElement(baseBtn);
+    }
+
+    private void CloseAllMenus()
+    {
+        Time.timeScale = 1f;
+        gameObject.SetActive(false);
+        settingPanel.SetActive(false);
+        input.XRIUI.Disable();
+    }
+
+    public void OnClick_OpenSettings()
+    {
+        SoundManager_K.Instance.PlaySFX(clickbtn);
+        settingPanel.SetActive(true);
+
+        SetSelectedUIElement(settingSilder);
+    }
+
+    public void OnClick_CloseSettings()
+    {
+        SoundManager_K.Instance.PlaySFX(clickbtn);
+        gameObject.SetActive(false);
+
+        SetSelectedUIElement(baseBtn);
+    }
+
+    public void OnClick_ExitGame()
+    {
+        SoundManager_K.Instance.PlaySFX(clickbtn);
+        Time.timeScale = 1f;
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void SetSelectedUIElement(GameObject element)
+    {
+        EventSystem.current.SetSelectedGameObject(element);
+    }
+}
